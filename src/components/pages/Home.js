b5754d412646daed6aa09bc7103cb978667621e9
Johonatan Seibel <globalsys.johonatan@willbank.com.br>
2023-02-04 12:00:22 -0300

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,8 +5,8 @@ import style from './css//Home.module.css'
 function Home(){
     return (
         <section className={style.home_container}>
-            <h1>Bem vindo ao <span>Share Account</span></h1>
-            <p>Faça o rateio de sua conta Aqui!</p>
+            <h1>Welcome to <span>Share Account</span></h1>
+            <p>Share your account here!</p>
             <LinkButton to="/order" text="New Order"/>
         </section>
     )
